d863517558c7bef929620ecd11ac3d8b8eaace9f
Chris Phillips <cminion@gmail.com>
2024-07-16 12:52:43 +0100

    updated html page to be cleaner

--- Frontend/index.js
+++ Frontend/index.js
@@ -11,7 +11,10 @@ const studentno = process.env.studno || "student01";
 const bookServiceUrl = process.env.url || "http://localhost:3001"
 
 function getPage(content, title) {
-  let toReturn = "<!--@licenseCopyright IBM Corp. 2020This source code is licensed under the Apache-2.0 license found in theLICENSE file in the root directory of this source tree.--><html><head>  <title>" + title + "</title>  <meta charset=\"UTF-8\"/>  <link rel=\"stylesheet\"        href=\"https://1.www.s81c.com/common/carbon-for-ibm-dotcom/tag/v1/latest/plex.css\"/>  <style>    /* Suppress custom element until styles are loaded */    cds-header:not(:defined) {      display: none;    }  </style>  <script type=\"module\" src=\"https://1.www.s81c.com/common/carbon/web-components/version/v2.10.0/accordion.min.js\"></script>  <script type=\"module\" src=\"https://1.www.s81c.com/common/carbon/web-components/tag/v2/latest/ui-shell.min.js\"></script></head>  <cds-header aria-label=\"IBM Platform Name\">    <cds-header-menu-button      button-label-active=\"Close menu\"      button-label-inactive=\"Open menu\"    ></cds-header-menu-button>    <cds-header-name href=\"/\" prefix=\"IBM\">Lab 1193 Reading list</cds-header-name>    <cds-header-nav menu-bar-label=\"IBM Lab 1193 Reading List\">      <cds-header-nav-item href=\"/\">Home</cds-header-nav-item>    </cds-header-nav>  </cds-header>  <BR><BR><BR><BR>  <body class=\"cds-theme-zone-white\">" + content + "</body></html>"
+
+  let toReturn = "<html>  <head>   " + title + "   <meta charset=\"UTF-8\">   <link rel=\"stylesheet\" href=\"https://1.www.s81c.com/common/carbon-for-ibm-dotcom/tag/v1/latest/plex.css\">   <style>     /* Suppress custom element until styles are loaded */     cds-header:not(:defined) {       display: none;     }   </style>   <script type=\"module\" src=\"https://1.www.s81c.com/common/carbon/web-components/version/v2.10.0/accordion.min.js\"></script>   <link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH\" crossorigin=\"anonymous\">   <script type=\"module\" src=\"https://1.www.s81c.com/common/carbon/web-components/version/v2.10.0/ui-shell.min.js\"></script>   <script type=\"module\" src=\"https://raw.githubusercontent.com/carbon-design-system/carbon/main/packages/grid/examples/preview/styles.scss\"></script> </head>  <body class=\"cds-theme-zone-white\">   <cds-header aria-label=\"IBM Platform Name\" role=\"banner\">     <cds-header-menu-button button-label-active=\"Close menu\" button-label-inactive=\"Open menu\" collapse-mode=\"responsive\"></cds-header-menu-button>     <cds-header-name href=\"/\" prefix=\"IBM\">Lab 1193 Reading list</cds-header-name>     <cds-header-nav menu-bar-label=\"IBM Lab 1193 Reading List\" role=\"navigation\">       <cds-header-nav-item href=\"/\" role=\"listitem\">Home</cds-header-nav-item>     </cds-header-nav>   </cds-header>    <div class=\"container\">     <div class=\"row-5\"><div class=\"col\">Header</div></div>     <div class=\"row-5\"><div class=\"col\">Header</div></div>     <div class=\"row\">       <div class=\"col\"> </div> <div class=\"col-10\"> " + content + "</div>   <div class=\"col\"> </div> </div> </div> </body>  </html>"
+
+
   console.log(toReturn);
   return toReturn;
 }
@@ -34,7 +37,7 @@ app.get('/', (req, res) => {
         "<cds-accordion>";
       users.forEach((e, i) => {
         console.log(e)
-        t = t + "<cds-accordion-item title=\"Book " + i + " - " + e.title + "\">" +
+        t = t + "<cds-accordion-item title=\"Book " + (i+1) + " - " + e.title + "\">" +
           "<Div class=title><h3>" + e.title + "</H3></Div>" +
           "<Div class=author>Author(s): <B>" + e.author + "</B></Div>" +
           "<Div class=bookcover><img src=https://chrisphillips-cminion.github.io/images/blank.svg></Div>" +
